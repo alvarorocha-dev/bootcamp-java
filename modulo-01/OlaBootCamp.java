8681118554db4ad8174d25e46a5872cebdd6db3b
Alvaro Rocha <alvarowebrj@gmail.com>
2026-05-06 21:05:12 -0300

    feat(modulo-01): adiciona primeiro programa Java do Bootcamp

---
+++ modulo-01/OlaBootCamp.java
@@ -0,0 +1,14 @@
+public class OlaBootCamp {
+
+    public static void main(String[] args) {
+        
+        String aluno = "Álvaro";
+        int modulo = 1;
+
+        System.out.println("== Bootcamp Java Backend ==");
+        System.out.println("Aluno: " + aluno);
+        System.out.println("Módulo atual: " + modulo);
+        System.out.println("Status: Em andamento");
+
+    }
+}
